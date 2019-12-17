d3e64ac48ac59a3a947edcb1b544682dd6c7ee1f
zinoopark09 <55022796+zinoopark09@users.noreply.github.com>
2019-12-17 09:18:16 +0900

    removed

--- main.py
+++
@@ -1,6 +0,0 @@
-
-from luigi import build, LuigiStatusCode
-
-from task import Validation, VennGraph, checkDependency, tree
-
-build([VennGraph(),Validation(),checkDependency(),tree(),checkDependency(dependency_to_check="cryptography")], local_scheduler=True, detailed_summary=False)
